e54c81645ed236a3406145c45dd822dfac759d27
Славян <slavyan@MacBook-Pro-Slavan.local>
2017-09-13 18:40:26 +0300

    Продолжение домашки к 2 уроку

--- Homework/SlavyanLesson2.playground/Contents.swift
+++ Homework/SlavyanLesson2.playground/Contents.swift
@@ -18,11 +18,21 @@ UInt32.max
 UInt64.max
 
 
+print("min значение Int8 = \(Int8.min)\nmax значение Int8 = \(Int8.max)")
+
+print("min значение Int16 = \(Int16.min)\nmax значение Int16 = \(Int16.max)")
+
+print("min значение Int32 = \(Int32.min)\nmax значение Int32 = \(Int32.max)")
+
+print("min значение Int64 = \(Int64.min)\nmax значение Int64 = \(Int64.max)")
+
+
+
 let ferstA = 16
 
 let ferstB = 32.16
 
-let ferstC :Float = 64
+let ferstC :Float = 64.32
 
 
 let summaInt = ferstA + Int(ferstB) + Int(ferstC)
@@ -32,5 +42,9 @@ let summaDouble = Double(ferstA) + ferstB + Double(ferstC)
 let summaFloat = Float(ferstA) + Float(ferstB) + ferstC
 
 
+print("Сумма трех костант в выражении Int = \(summaInt)")
 
+print("Сумма трех констант в выражении Double = \(summaDouble)")
+
+print("Сумма трех констант в выражении Float = \(summaFloat)")
 
