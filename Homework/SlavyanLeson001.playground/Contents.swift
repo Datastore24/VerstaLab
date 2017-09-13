de64848595aeb9658e90565e52d0a2520fc12fbd
Славян <slavyan@MacBook-Pro-Slavan.local>
2017-09-13 15:06:37 +0300

    ДоМашка

---
+++ Homework/SlavyanLeson001.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+//: import UIKit
+
+// var str = "Pervyi Urok"
+
+let nameStudent = "Ivan"
+
+let secondNameStudent = "Ivanovich"
+
+let familiaStudent = "Ivanov"
+
+let vozrastStudent = "23 years"
+
+let fackultetStudent = "Programming for SWIFT"
+
+var age = "23"
+
+let newAge = Int(age)! + 1
+
+print("Hello \(nameStudent)")
+print("Hello, \(nameStudent) \(secondNameStudent) \(familiaStudent)! \n My age is: \(newAge)")
+print(fackultetStudent)
+print("HappyBirthday OLDAge: \(vozrastStudent) AND NEW AGE: \(newAge) years")
+
+
+
+
+
+
+
+
