1416e866d6ffe522c9fa9a7a70f7498dca128ca8
Славян <slavyan@MacBook-Pro-Slavan.local>
2017-09-13 18:05:48 +0300

    Lesson2

---
+++ Homework/SlavyanLesson2.playground/Contents.swift
@@ -0,0 +1,36 @@
+//:Lesson2
+
+Int8.min
+Int8.max
+
+Int16.min
+Int16.max
+
+Int32.min
+Int32.max
+
+Int64.min
+Int64.max
+
+UInt8.max
+UInt16.max
+UInt32.max
+UInt64.max
+
+
+let ferstA = 16
+
+let ferstB = 32.16
+
+let ferstC :Float = 64
+
+
+let summaInt = ferstA + Int(ferstB) + Int(ferstC)
+
+let summaDouble = Double(ferstA) + ferstB + Double(ferstC)
+
+let summaFloat = Float(ferstA) + Float(ferstB) + ferstC
+
+
+
+
