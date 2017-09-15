b364d750b6e1a817d2bf269fa67bddbb151967c5
Кирилл Ковыршин <co@datastore24.ru>
2017-09-15 14:21:26 +0300

    Показывал

--- Homework/Slavyan_Lesson03.playground/Contents.swift
+++ Homework/Slavyan_Lesson03.playground/Contents.swift
@@ -1,5 +1,64 @@
 // Lesson 3 Tuples
 
+
+//Развертывание и именование тюпла
+
+// let testTuple :  (name2, age2, auth2) = ("Kirill", 30, true)
+
+//v1
+let testTuple = ("Kirill", 30, true)
+var (name, age, auth) = testTuple
+
+
+//v2
+let (name2, age2, auth2) = ("Kirill", 30, true)
+
+//v3
+let test = (name: "Kirill", age: 30, auth: true)
+
+//Tuple Optional Type
+var test3: (name: String,  age: Int?, auth: Bool?) = (name: "", age: nil, auth: nil)
+
+//
+
+if test3.age != nil {
+    print(test3.age!)
+}else{
+    print("AGE NIL")
+}
+
+if let test = test3.age {
+    print(test)
+} else {
+    print("NIL")
+}
+
+//
+
+let apples: Int? = nil
+
+if let apples = apples {
+    print(apples)
+}else{
+    print("APPLES NIL")
+}
+
+
+
+
+name2
+age2
+auth2
+
+
+name
+age
+auth
+
+
+//
+
+
 let myMaxParametrs = (pushUps:10, pullUps:2, squats:25)
 
 //var (push, pull, squats) = (10, 2, 25)
@@ -54,6 +113,16 @@ intermediatToReplaceKiraParametrs.0 = myMaxParametrs2.0
 intermediatToReplaceKiraParametrs.pullUpsKira = myMaxParametrs2.1
 intermediatToReplaceKiraParametrs.2 = myMaxParametrs2.squats
 
+//Вариант решения по ТЗ
+let kiraMaxParams = (pushUps:30, pullUps:4, squats:50)
+
+var kiraToSlavyanParams = kiraMaxParams
+
+kiraToSlavyanParams = myMaxParametrs
+
+
+//
+
 
 
 kiraMaxParametrs
@@ -62,7 +131,7 @@ intermediatToReplaceKiraParametrs
 
 
 
-let forDifferenceTuples = (differencePushUps: kiraMaxParametrs.0 - myMaxParametrs2.0, differencePullUps: kiraMaxParametrs.1 - myMaxParametrs2.1, differenceSquats: kiraMaxParametrs.2 - myMaxParametrs2.2)
+let forDifferenceTuples = (differencePushUps: kiraToSlavyanParams.pushUps - myMaxParametrs2.pushUps, differencePullUps: kiraMaxParametrs.1 - myMaxParametrs2.1, differenceSquats: kiraMaxParametrs.2 - myMaxParametrs2.2)
 
 
 print ("Та самая разница ТЮПЛОВ \(forDifferenceTuples)")
