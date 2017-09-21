af31a25d4652dceb3c5faab2998affee5e4f84cf
Vyacheslav <5435836@mail.ru>
2017-09-21 14:10:10 +0300

    final

--- Homework/Slavyan_Lesson 06.playground/Contents.swift
+++ Homework/Slavyan_Lesson 06.playground/Contents.swift
@@ -1,6 +1,7 @@
-//Slavyan Lesson 6
+
 import Foundation
 
+//Slavyan Lesson 6
 var str = "c"
 
 str += "a"
@@ -20,7 +21,7 @@ var str2 = str1
 str1
 str2
 
-str1 = "b"
+//str1 = "b"
 
 str1
 str2
@@ -33,6 +34,7 @@ let char1 : Character = "x"
 for c in "Hello world!" {
     print(c)
 }
+упразнено
 */
 
 str1.append(char1)
@@ -40,8 +42,11 @@ str1.append(char1)
 //(str1 as NSString).length
 //NSString(string: str1).length
 
+let moto = "\u{1F3CD}"
 
-let heart = "\u{1F496}"
+let heart = "\u{1F496}\u{2B50}"
+
+let rockBlack = "\u{1F918}\u{1F3FF}"
 
 let eAcute: Character = "\u{E9}"
 let combinedEAcute: Character = "e\u{301}"
@@ -53,6 +58,115 @@ let funString = "what is this? -> \(fun)"
 
 funString
 
-//countE countElements (funString)
+//countElements (funString)
+NSString(string: funString).length
+
+if funString == "aa" {
+    
+} else {
+    print("not eqtual")
+}
+
+funString.hasPrefix("what")
+
+funString.hasSuffix("ъ")
+
+funString.hasSuffix("ъ\u{301}\u{20dd}")
+
+funString.hasSuffix("ъ\u{20dd}\u{301}")
 
 
+
+//Zad 1
+
+let first  = "1423"
+let second = "1423NW"
+let third  = "3241"
+let fourth = "14NW23"
+let fifth  = "14233241"
+
+//var firstConv = Int()
+//
+//if firstConv == Int(first) ?? 0 {
+//    firstConv += 0
+//} else if firstConv == Int(second) ?? 0 {
+//    firstConv += Int(second)
+//} else if firstConv == Int(third) ?? 0 {
+//    firstConv += Int(third)!
+//} else if firstConv == Int(fourth) ?? 0 {
+//    firstConv += Int(fourth)!
+//} else if firstConv == Int(fifth) ?? 0 {
+//    firstConv += Int(fifth)!
+//}
+
+
+
+//if firstConv == Int(first) ?? 0 {
+//    firstConv += 0
+//} else if firstConv == Int(second) ?? 0 {
+//    firstConv += 0
+//} else if firstConv == Int(third) ?? 0 {
+//    firstConv += 0
+//} else if firstConv == Int(fourth) ?? 0 {
+//    firstConv += 0
+//} else if firstConv == Int(fifth) ?? 0 {
+//    firstConv += 0 {
+//        print (firstConv)
+//    }
+//}
+
+//var auxFirst = Int(first) ?? 0
+//var auxSecond = Int(second) ?? 0
+//var auxThird = Int(third) ?? 0
+//var auxFourth = Int(fourth) ?? 0
+//var auxFifth = Int(fifth) ?? 0
+
+var sum2 = 0
+
+sum2 += Int(first) ?? 0
+sum2 += Int(second) ?? 0
+sum2 += Int(third) ?? 0
+sum2 += Int(fourth) ?? 0
+sum2 += Int(fifth) ?? 0
+
+
+//print("\(Int(first)) + \(Int(second)) + \(Int(third)) + \(Int(fourth)) + \(Int(fifth)) = \(sum2)")
+
+
+//var sum = auxFifth + auxSecond + auxThird + auxFourth + auxFifth
+//print ("\(auxFirst)  + \(auxSecond) + \(auxThird) + \(auxFourth) + \(auxFifth) = \(sum) ")
+
+
+//let second2: Int? = 0
+//
+//var bbb = Int(first)!
+//bbb += second2 ?? 0
+
+
+//Zad 2
+
+let firstTwo = ("\u{1F44A}\u{1F3FB}\u{1F43A}\u{1F30F}\u{264E}\u{1F1F7}\u{1F1FA}")
+
+NSString(string: firstTwo).length
+firstTwo.characters.count
+
+
+
+//Zad 3
+
+var abc = "abcdefghijklmnopqrstuvwxyz"
+
+let symbol : Character = "n"
+
+let char = abc.characters
+
+var index = 0
+
+for value in char {
+    
+    //print("index of \(value) is \(index)")
+    if value == symbol {
+        print("index of \(symbol) is \(index)")
+    }
+    index += 1
+}
